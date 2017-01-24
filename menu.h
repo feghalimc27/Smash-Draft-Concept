e5518f0a9c3a2b8078dd955876307c6106ebb66c
Mario Feghali <mibikin@gmail.com>
2017-01-24 15:16:48 -0700

    Added draft implementation for Smash 4, still needs to be tested

--- menu.h
+++ menu.h
@@ -5,23 +5,58 @@
 #include <vector>
 #include <string>
 
-void displayRoster(int c) {
+void actDraft(std::vector<Player> &players) {
     std::ifstream roster;
     std::vector<std::string> vRoster;
     std::string entry;
+    bool invalidPick;
+    int c, numPlayers, totalChars, perTeam, choice;
+
+    totalChars = 0;
+
+    numPlayers = players.size() + 1;
+
+    cout << "Please select a game: \n1)Smash 4" << endl;
+    cin >> c;
+    cin.ignore();
 
     if(c == 1) {
         roster.open("smash4roster.txt");
         while(!roster.eof()) {
             getline(roster, entry);
-            cout << entry;
             vRoster.push_back(entry);
+            totalChars++;
         }
-        for(int i = 0; i < vRoster.size(); ++i) {
-            cout << vRoster[i] << endl;;
+        c = 999;
+        while(c > totalChars/numPlayers) {
+            cout << "How many characters per team? " << endl;
+            cin >> c;
+            cin.ignore();
         }
-        for(int i = 0; i < vRoster.size(); ++i) {
+        perTeam = c;
+        invalidPick = true;
+        for(int i = 0; i < numPlayers * perTeam;) {
+            for(int j = 0; j < vRoster.size(); j++) {
+                cout << vRoster[j] << endl;
+            }
+            for(int k = 0; k < players.size(); k++) {
+                cout << players[k].getName() << "'s turn.\n";
+                cout << "Please make a selection:" << endl;
+                choose4:
+                cin >> choice;
+                choice--;
+                if(vRoster[choice] != " ") {
+                    players[k].draftFighter(vRoster[choice]);
+                    vRoster[choice] = " ";
+                }
+                else {
+                    cout << "Character drafted. Please try again." << endl;
+                    goto choose4;
+                }
+                i++;
+            }
         }
+        cout << totalChars;
         roster.close();
         return;
     }
@@ -45,12 +80,13 @@ void newDraft(int numPlayers, vector<Player> &players) {
         cout << " Seed: " << players[i].getSeed();
         cout << " Draft Position: " << players[i].getPosition() << endl;
     }
+    cin.ignore();
     cout << endl << "Begin draft? (Y/n)" << endl;
     while(choice != 'n') {
         cin >> choice;
         if(choice == 'Y' || choice == 'y') {
             system("cls");
-            displayRoster(1);
+            actDraft(players);
             return;
         }
         else if(choice == 'N' || choice == 'n') {
